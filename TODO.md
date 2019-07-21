a597f7342a3ed7e647e97e64c544159839718d3c
Luis Martinez Suarez <luis@Luiss-MBP.fritz.box>
2019-07-21 19:16:20 +0200

    Add emojis fetched from lambda function

--- TODO.md
+++ TODO.md
@@ -2,8 +2,8 @@
 
 Task 1: Add pet and age column to the database and allow users to update these fields on their profile. DONE
 
-Task 2: Implement token flow as we have done so far for /register end point as well.
+Task 2: Implement token flow as we have done so far for /register end point as well. DONE
 
-Task 3: Add Sign out functionality where you revoke the token when a user signs out of the app
+Task 3: Add Sign out functionality where you revoke the token when a user signs out of the app DONE
 
 Task 4 (Bonus): You may notice a lot of repeated code (especially when it comes to fetch()). How can you improve this? Try to refactor the code and perhaps also improve the logic to make the app even better!
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,16 +1,47 @@
-import React from 'react';
+import React from "react";
 
-const Rank = ({ name, entries }) => {
-  return (
-    <div>
-      <div className='white f3'>
-        {`${name}, your current entry count is...`}
+class Rank extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      emoji: ""
+    };
+  }
+
+  componentDidMount() {
+    this.generateEmoji(this.props.entries);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (
+      prevProps.entries === this.props.entries &&
+      prevProps.name === this.props.name
+    )
+      return null;
+    this.generateEmoji(this.props.entries);
+  }
+
+  generateEmoji = entries => {
+    fetch(
+      `https://26wl06yz11.execute-api.eu-central-1.amazonaws.com/prod/rank?rank=${entries}`
+    )
+      .then(resp => resp.json())
+      .then(data => this.setState({ emoji: data.input }))
+      .catch(err => console.log(err));
+  };
+
+  render() {
+    const { name, entries } = this.props;
+    return (
+      <div>
+        <div className="white f3">
+          {`${name}, your current entry count is...`}
+        </div>
+        <div className="white f1">{entries}</div>
+        <div className="white f3">{`Rank Badge: ${this.state.emoji}`}</div>
       </div>
-      <div className='white f1'>
-        {entries}
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Rank;
